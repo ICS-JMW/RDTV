c57dce31992538826c152e57735c4045945db4c1
echometerain <echometer@disroot.org>
2024-06-03 18:55:52 -0400

    spring part 2

---
+++ src/main/java/jmw/rdtv/RdtvApplication.java
@@ -0,0 +1,13 @@
+package jmw.rdtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdtvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdtvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jmw/rdtv/RdtvApplicationTests.java
@@ -0,0 +1,13 @@
+package jmw.rdtv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdtvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
