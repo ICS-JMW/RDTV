22bf9c49aad3d4712d2b0d4c1f02d23ce4fcc80b
antinomy <arekbyu@gmail.com>
2024-06-06 08:55:47 -0700

    Needs more implementation in order to make image work

---
+++ src/main/java/jmw/rdtv/Medium/medium.java
@@ -0,0 +1,12 @@
+package jmw.rdtv.Medium;
+
+import java.awt.Graphics;
+
+public abstract class medium {
+    String Name;
+    String Description;
+    String Begin;
+    String End;
+    public void draw(Graphics g){}
+    
+}
